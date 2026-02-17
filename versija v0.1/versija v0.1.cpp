0b59e280dc8c72c0a53a6592b680bdd1cd6b911d
SynthyDev <vytenis.juknenai@gmail.com>
2026-02-17 19:16:29 +0200

    Prideta mediana

--- versija v0.1/versija v0.1.cpp
+++ versija v0.1/versija v0.1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iomanip>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ struct Studentas {
     vector<int> paz;
     int egz;
     double rez;
+    double medrez;
 };
 
 void inputas(vector<Studentas> &grupe);
@@ -52,6 +54,15 @@ void inputas(vector<Studentas> &grupe)
 
         A.rez = (sum * 1.0 / n) * 0.4 + A.egz * 0.6;
 
+        if (A.paz.size() % 2 == 0)
+        {
+            A.medrez = (A.paz[floor(A.paz.size()*1.0/2)] + ceil(A.paz.size()*1.0/2)) / 2;
+        }
+        else
+        {
+            A.medrez = A.paz[floor(A.paz.size()/2)];
+        }
+
         grupe.push_back(A);
 
         A.paz.clear();
@@ -63,7 +74,7 @@ void outputas(const vector<Studentas> &grupe)
         cout << "-------------------------------------------------------------------" << endl;
         cout << left << setw(15) << "Vardas"
          << left << setw(15) << "Pavarde"
-         << right << setw(10) << fixed << setprecision(2) << "Rezultatas"
+         << right << setw(10) << fixed << setprecision(2) << "Rezultatas (Vid.)  /  Rezultatas (Med.)"
          << endl;
         cout << "-------------------------------------------------------------------" << endl;
     for (const auto &A : grupe)
@@ -71,6 +82,7 @@ void outputas(const vector<Studentas> &grupe)
         cout << left << setw(15) << A.vardas
              << left << setw(15) << A.pavarde
              << right << setw(10) << fixed << setprecision(2) << A.rez
+             << right << setw(20) << A.medrez
              << endl;
     }
     cout << "-------------------------------------------------------------------" << endl;
