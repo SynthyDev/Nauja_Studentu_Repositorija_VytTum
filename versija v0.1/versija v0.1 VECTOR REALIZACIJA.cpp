6d1ee3ef4d58c7d189cbe229be6c1d67d12e7be3
SynthyDev <vytenis.juknenai@gmail.com>
2026-02-18 20:35:24 +0200

    Vector užduoties failas baigtas su pasirinkimu ir randominiais skaičiais. Masyvų failas dar tuščias.

--- versija v0.1/versija v0.1 VECTOR REALIZACIJA.cpp
+++ versija v0.1/versija v0.1 VECTOR REALIZACIJA.cpp
@@ -5,8 +5,15 @@
 #include <cmath>
 #include <algorithm>
 #include <random>
+#include <ctime>
 
 using namespace std;
+
+const vector<string> mvardai = {"Vytenis", "Tomas", "Jonas", "Matas", "Simas", "Mantas", "Arnas"};
+const vector<string> fvardai = {"Eglė", "Viktorija", "Vakarė", "Inga", "Ema", "Marija", "Janina"};
+const vector<string> pavardes = {"Mačerausk", "Jankausk", "Kazlausk", "Švilpausk", "Drugėliausk", "Briedausk"};
+const vector<string> mgalunes = {"as", "aitis"};
+const vector<string> fgalunes = {"ienė", "aitė", "ienė", "ytė"};
 
 struct Studentas {
     string vardas = "A", pavarde = "BB";
@@ -17,33 +24,41 @@ struct Studentas {
 };
 
 void inputas(vector<Studentas> &grupe);
+void randominputas(vector<Studentas> &grupe, bool ArGeneruotiVardus);
 void outputas(const vector<Studentas> &grupe);
+string lytgen();
+string randomvardas(string lytis);
+string randompavarde(string lytis);
 
 int main()
 {
+    srand(time(NULL));
+    
     vector<Studentas> grupe;
 
     int pasirinkimas = 4;
     cout << "Kokios norite ivesties? (1 - ranka, 2 - generuoti tik pažymius, " << endl
-    << "3 - generuoti studentų vardus, pavardės ir pažymius, 4 arba kitas simbolis - baigti darbą)" << endl;
+    << " 3 - generuoti studentų vardus, pavardės ir pažymius, 4 arba kitas simbolis - baigti darbą)" << endl;
     cin >> pasirinkimas;
 
-    if (pasirinkimas == 1) // ranka
+    if (pasirinkimas == 1)
     {
         inputas(grupe);
         outputas(grupe);
     }
-    else if (pasirinkimas == 2) // generuoti tik pazymius
+    else if (pasirinkimas == 2)
     {
-        cout << "no2" << endl;
+        randominputas(grupe, false);
+        outputas(grupe);
     }
-    else if (pasirinkimas == 3) // generuoti vardus, pavardes, ir pazymius
+    else if (pasirinkimas == 3)
     {
-        cout << "no3" << endl;
+        randominputas(grupe, true);
+        outputas(grupe);
     }
-    else // baigti darba
+    else
     {
-        cout << "no4" << endl;
+        cout << "darbas baigtas" << endl;
     }
 
     return 0;
@@ -92,54 +107,186 @@ void inputas(vector<Studentas> &grupe)
         cout << "Iveskite egzamino rezultata: ";
         cin >> A.egz;
 
-        A.rez = (sum * 1.0 / A.paz.size()) * 0.4 + A.egz * 0.6;
-
-        sort(A.paz.begin(), A.paz.end());
-
-        if (A.paz.size() % 2 == 0)
+        if (A.paz.size() > 0)
         {
-            A.medrez = (A.paz[A.paz.size()*1.0/2.0 - 1] + A.paz[A.paz.size()*1.0/2.0]) / 2.0;
-        }
-        else
-        {
-            A.medrez = A.paz[floor(A.paz.size()/2)];
+            A.rez = (sum * 1.0 / A.paz.size()) * 0.4 + A.egz * 0.6;
+
+            sort(A.paz.begin(), A.paz.end());
+
+            int n = A.paz.size();
+            if (n % 2 == 0)
+            {
+                A.medrez = (A.paz[n/2 - 1] + A.paz[n/2]) / 2.0;
+            }
+            else
+            {
+                A.medrez = A.paz[n/2];
+            }
         }
 
         grupe.push_back(A);
         A.paz.clear();
 
         cout << "Ar baigete vesti mokinius? (y/n): ";
-            cin >> ats;
-            if (ats == "y")
-            {
-                break;
-            }
-            else if (ats == "n")
-            {
-                cout << "Veskite toliau: ";
-            }
-            else
-            {
-                cout << "NETEISINGA IVESTIS, programa veikia toliau" << endl;
-            }
+        cin >> ats;
+        if (ats == "y")
+        {
+            break;
+        }
+        else if (ats == "n")
+        {
+            cout << "Veskite toliau: ";
+        }
+        else
+        {
+            cout << "NETEISINGA IVESTIS, programa veikia toliau" << endl;
+        }
     }
 }
 
 void outputas(const vector<Studentas> &grupe)
 {
-        cout << "-------------------------------------------------------------------" << endl;
-        cout << left << setw(15) << "Vardas"
-         << left << setw(15) << "Pavarde"
-         << right << setw(10) << fixed << setprecision(2) << "Rezultatas (Vid.)  /  Rezultatas (Med.)"
+    cout << "-------------------------------------------------------------------" << endl;
+    cout << left << setw(20) << "Vardas"
+         << setw(20) << "Pavarde"
+         << setw(20) << "Rezultatas (Vid.) /"
+         << setw(20) << " Rezultatas (Med.)"
          << endl;
-        cout << "-------------------------------------------------------------------" << endl;
+    cout << "-------------------------------------------------------------------" << endl;
+
     for (const auto &A : grupe)
     {
-        cout << left << setw(15) << A.vardas
-             << left << setw(15) << A.pavarde
-             << right << setw(10) << fixed << setprecision(2) << A.rez
-             << right << setw(20) << A.medrez
+        cout << left << setw(20) << A.vardas
+             << setw(20) << A.pavarde
+             << right << setw(20) << fixed << setprecision(2) << A.rez
+             << setw(20) << A.medrez
              << endl;
     }
+
     cout << "-------------------------------------------------------------------" << endl;
+}
+
+void randominputas(vector<Studentas> &grupe, bool ArGeneruotiVardus)
+{
+    Studentas A;
+    
+    string lytis;
+    string ats;
+
+    if (ArGeneruotiVardus == true)
+    {
+        int zmonsk = rand() % 100 + 1;
+
+        for(int iii = 0; iii < zmonsk; iii++)
+        {
+            int sum = 0;
+
+            lytis = lytgen();
+            A.vardas = randomvardas(lytis);
+            A.pavarde = randompavarde(lytis);
+
+            int pazsk = rand() % 100 + 1;
+
+            for (int ii = 0; ii < pazsk; ii++)
+            {
+                int temp = rand() % 10 + 1;
+                A.paz.push_back(temp);
+                sum += temp;
+            }
+
+            A.egz = rand() % 10 + 1;
+
+            if (A.paz.size() > 0)
+            {
+                A.rez = (sum * 1.0 / A.paz.size()) * 0.4 + A.egz * 0.6;
+
+                sort(A.paz.begin(), A.paz.end());
+
+                int n = A.paz.size();
+                if (n % 2 == 0)
+                    A.medrez = (A.paz[n/2 - 1] + A.paz[n/2]) / 2.0;
+                else
+                    A.medrez = A.paz[n/2];
+            }
+
+            grupe.push_back(A);
+            A.paz.clear();
+        }
+    }
+    else
+    {
+        while (true)
+        {
+            int sum = 0;
+
+            cout << "Vardas ir pavarde: ";
+            cin >> A.vardas >> A.pavarde;
+
+            int pazsk = rand() % 100 + 1;
+
+            for (int i = 0; i < pazsk; i++)
+            {
+                int temp = rand() % 10 + 1;
+                A.paz.push_back(temp);
+                sum += temp;
+            }
+
+            A.egz = rand() % 10 + 1;
+
+            if (A.paz.size() > 0)
+            {
+                A.rez = (sum * 1.0 / A.paz.size()) * 0.4 + A.egz * 0.6;
+
+                sort(A.paz.begin(), A.paz.end());
+
+                int n = A.paz.size();
+                if (n % 2 == 0)
+                    A.medrez = (A.paz[n/2 - 1] + A.paz[n/2]) / 2.0;
+                else
+                    A.medrez = A.paz[n/2];
+            }
+
+            grupe.push_back(A);
+            A.paz.clear();
+
+            cout << "Ar baigete vesti zmones? (y/n): ";
+            cin >> ats;
+
+            if (ats == "y")
+                break;
+            else if (ats == "n")
+                cout << "Veskite toliau: ";
+            else
+                cout << "NETEISINGA IVESTIS, programa veikia toliau" << endl;
+        }
+    }
+}
+
+string randomvardas(string lytis)
+{
+    if (lytis == "mot")
+        return fvardai[rand() % fvardai.size()];
+    else
+        return mvardai[rand() % mvardai.size()];
+}
+
+string randompavarde(string lytis)
+{
+    int index = rand() % pavardes.size();
+    string galune;
+
+    if (lytis == "mot")
+        galune = fgalunes[rand() % fgalunes.size()];
+    else
+        galune = mgalunes[rand() % mgalunes.size()];
+
+    return pavardes[index] + galune;
+}
+
+string lytgen()
+{
+    if (rand() % 2 == 0)
+        return "vyr";
+    else
+        return "mot";
 }
