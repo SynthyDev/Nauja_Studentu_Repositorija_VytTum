08fb0917c9a5ed116fc63bb86330b4855750a976
SynthyDev <vytenis.juknenai@gmail.com>
2026-03-27 13:59:03 +0200

    pabaigta pirminė deque versija

--- deque/studentai.h
+++ deque/studentai.h
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-const vector<string> mvardai = {"Vytenis", "Tomas", "Jonas", "Matas", "Simas", "Mantas", "Arnas"};
-const vector<string> fvardai = {"Egle", "Viktorija", "Vakare", "Inga", "Ema", "Marija", "Janina"};
-const vector<string> pavardes = {"Macerausk", "Jankausk", "Kazlausk", "Svilpausk", "Drugeliausk", "Briedausk"};
-const vector<string> mgalunes = {"as", "aitis"};
-const vector<string> fgalunes = {"iene", "aite", "yte"};
+const deque<string> mvardai = {"Vytenis", "Tomas", "Jonas", "Matas", "Simas", "Mantas", "Arnas"};
+const deque<string> fvardai = {"Egle", "Viktorija", "Vakare", "Inga", "Ema", "Marija", "Janina"};
+const deque<string> pavardes = {"Macerausk", "Jankausk", "Kazlausk", "Svilpausk", "Drugeliausk", "Briedausk"};
+const deque<string> mgalunes = {"as", "aitis"};
+const deque<string> fgalunes = {"iene", "aite", "yte"};
 
 struct Studentas {
     string vardas = "A", pavarde = "BB";
-    vector<int> paz;
+    deque<int> paz;
     int egz;
     double rez;
     double medrez;
@@ -21,24 +21,24 @@ const string irasymo_failas = "kursiokai.txt";
 const string irasymo_failas_geras = "kursiokai_geri.txt";
 const string irasymo_failas_blogas = "kursiokai_blogi.txt";
 
-void inputas(vector<Studentas> &grupe);
-void failinputas(vector<Studentas> &grupe, string reading);
-void randominputas(vector<Studentas> &grupe, bool ArGeneruotiVardus);
-void outputas(const vector<Studentas> &grupe);
-void failoutputas(const vector<Studentas> &grupe, string writing);
+void inputas(deque<Studentas> &grupe);
+void failinputas(deque<Studentas> &grupe, string reading);
+void randominputas(deque<Studentas> &grupe, bool ArGeneruotiVardus);
+void outputas(const deque<Studentas> &grupe);
+void failoutputas(const deque<Studentas> &grupe, string writing);
 void failgeneravimas();
-void genirasymas(vector<Studentas> &grupe, vector<Studentas> &protingi, vector<Studentas> &neprotingi,
+void genirasymas(deque<Studentas> &grupe, deque<Studentas> &protingi, deque<Studentas> &neprotingi,
     string writing_good, string writing_bad, int rik_pasirinkimas);
 string lytgen();
 string randomvardas(string lytis);
 string randompavarde(string lytis);
-void rikiuoti(vector<Studentas> &grupe, int pasirinkimas);
+void rikiuoti(deque<Studentas> &grupe, int pasirinkimas);
 bool cmpVardas(const Studentas &a, const Studentas &b);
 bool cmpPavarde(const Studentas &a, const Studentas &b);
 bool cmpRez(const Studentas &a, const Studentas &b);
 bool cmpMedrez(const Studentas &a, const Studentas &b);
 bool cmpEgz(const Studentas &a, const Studentas &b);
-void skirstyti(const vector<Studentas>& grupe, vector<Studentas>& protingi, vector<Studentas>& neprotingi);
+void skirstyti(const deque<Studentas>& grupe, deque<Studentas>& protingi, deque<Studentas>& neprotingi);
 void logResults(const string& container, int size,
                 double read_t, double sort_t, double split_t);
                 
