2cbc22bb9d82f8c17d325547159fbd6a64ded21b
SynthyDev <vytenis.juknenai@gmail.com>
2026-05-16 17:09:11 +0300

    pabaigti unit testai

--- vector/testing.cpp
+++ vector/testing.cpp
@@ -79,8 +79,7 @@ TEST_CASE("Studentas copy assignment") {
 
 TEST_CASE("Studentas move konostruktorius") {
     Studentas a = makeStudent();
-    Studentas b;
-    b = a;
+    Studentas b(move(a));
 
     REQUIRE(b.getVardas() == "Jonas");
     REQUIRE(b.getPavarde() == "Jonaitis");
@@ -95,11 +94,72 @@ TEST_CASE("Studentas move konostruktorius") {
     b.skaiciuoti();
 
     REQUIRE(b.getRez() == 8.00);
-    REQUIRE(a.getRez() == 7.60);
+    REQUIRE(a.getRez() == 0.80); // del to, kad a tampa tuscias ir vienintelis pazymys yra 2
 }
 
+TEST_CASE("Studentas move assignment") {
+    Studentas a = makeStudent();
+    Studentas b;
+    b = move(a);
 
 
+    REQUIRE(b.getVardas() == "Jonas");
+    REQUIRE(b.getPavarde() == "Jonaitis");
+    REQUIRE(b.getPaz() == std::vector<int>{10, 9});
+    REQUIRE(b.getEgz() == 8);
+    REQUIRE(b.getRez() == 8.60);
+    REQUIRE(b.getMedrez() == 8.60);
+
+    b.addPaz(5);
+    a.addPaz(2);
+    a.skaiciuoti();
+    b.skaiciuoti();
+
+    REQUIRE(b.getRez() == 8.00);
+    REQUIRE(a.getRez() == 0.80); // del to, kad a tampa tuscias ir vienintelis pazymys yra 2
+    REQUIRE(a.getVardas().empty());
+    REQUIRE(a.getPavarde().empty());
+    REQUIRE(a.getEgz() == 0);
+}
+
+TEST_CASE("Studentas move a -> a") {
+    Studentas a = makeStudent();
+    a = move(a);
+
+    REQUIRE(a.getVardas() == "Jonas");
+    REQUIRE(a.getPavarde() == "Jonaitis");
+    REQUIRE(a.getPaz() == std::vector<int>{10, 9});
+    REQUIRE(a.getEgz() == 8);
+    REQUIRE(a.getRez() == 8.60);
+    REQUIRE(a.getMedrez() == 8.60);
+
+    REQUIRE(a.getRez() == 8.60); // del to, kad a tampa tuscias ir vienintelis pazymys yra 2
+}
+
+TEST_CASE("Studentas output operator >>") {
+    Studentas s = makeStudent();
+
+    std::ostringstream out;
+    out << s;
+
+    std::string result = out.str();
+
+    REQUIRE(result.find("Jonas") != std::string::npos);
+    REQUIRE(result.find("Jonaitis") != std::string::npos);
+    REQUIRE(result.find("8.60") != std::string::npos);
+}
+
+TEST_CASE("Studentas input operator >>") {
+    std::istringstream input("Jonas Jonaitis 10 9 8");
+
+    Studentas s;
+    input >> s;
+
+    REQUIRE(s.getVardas() == "Jonas");
+    REQUIRE(s.getPavarde() == "Jonaitis");
+    REQUIRE(s.getPaz() == std::vector<int>{10, 9});
+    REQUIRE(s.getEgz() == 8);
+}
 
 // output and input operators !!!
 // 
